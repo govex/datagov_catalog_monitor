0adf4bfd97c9e50d5c50e0b98afb285e41529843
technickle <github@nicklin.info>
2025-02-11 15:04:34 -0500

    massive updates; save changes locally

--- scripts/analyze_datagov_catalog/daily_statistics_polars.py
+++ scripts/analyze_datagov_catalog/daily_statistics_polars.py
@@ -10,7 +10,7 @@ import time
 
 logging.basicConfig(
     level=logging.DEBUG,
-    format="%(asctime)s.%(msecs)03d %(levelname)-8s %(name)s: %(message)s",
+    format="%(asctime)s.%(msecs)03d %(levelname)-8s| %(message)s",
     datefmt="%H:%M:%S"
 )
 _script_start = time.time()
@@ -31,18 +31,19 @@ local_config = {
 with open("excluded_organizations.csv", "r", encoding="utf-8") as f:
     excluded_organizations = f.read().splitlines()
 
-logging.debug(f"excluded organizations: {excluded_organizations}")
+logging.debug(f"excluded organizations: {len(excluded_organizations)}")
 
 # %%
 # functions
 
-# get the most recent catalog folder
-def get_recent_catalog_folder(root_catalog_folder: str = None, age: int = 0) -> str:
+# get the most recent catalog folders going back the specified number of cycles
+# cycles usually means days but that's not a strict rule;
+def get_recent_catalog_folders(root_catalog_folder: str = None, cycles: int = 1) -> str:
     if root_catalog_folder:
         folders = [os.path.join(root_catalog_folder, f) for f in os.listdir(root_catalog_folder) if os.path.isdir(os.path.join(root_catalog_folder, f))]
         folders.sort(key=lambda x: os.path.basename(x), reverse=True)
         if folders:
-            return folders[age]
+            return folders[:cycles+1]
     return None
 
 # get the list of json files in the folder
@@ -64,124 +65,80 @@ def get_json(file_path: str = None) -> list | dict:
             return json.load(file)
     return {}
 
-# scan a folder for ndson files and check for improper line breaks
-# the function returns True if any line breaks are found
-def check_ndson_for_line_breaks(folder_paths: list[str]) -> bool:
-    return_value = False
-    for folder_path in folder_paths:
-        for root, dirs, files in os.walk(folder_path):
-            for file_name in files:
-                if file_name.endswith(".ndjson"):
-                    file_path = os.path.join(root, file_name)
-                    with open(file_path, "r", encoding="utf-8") as f:
-                        for i, line in enumerate(f, start=1):
-                            try:
-                                json.loads(line.strip())
-                            except json.JSONDecodeError:
-                                logging.error(f"Invalid line break in {file_path}, line {i}")
-                                return_value = True
-    return return_value
-
-def filter_catalog(catalog: pl.LazyFrame, excluded_organizations: list[str]) -> pl.LazyFrame:
+# filter the catalog to remove duplicates and excluded organizations
+def filter_catalog(catalog: pl.LazyFrame, excluded_organizations: list[str] = []) -> pl.LazyFrame:
     return catalog \
         .unique(subset=["id"]) \
         .filter(
             ~pl.col("organization").struct.field("id").is_in(excluded_organizations)
         )
 
-# TODO: implement
-def collect_catalog_statistics(catalog: pl.LazyFrame) -> dict:
-    statistics = {}
+# collect statistics on the catalog; assumes any filtering has already been done
+def collect_catalog_info(catalog: pl.LazyFrame) -> dict:
+    catalog_info = {}
 
+    catalog_counts_by_organization = catalog \
+        .group_by("organization") \
+        .agg([
+            pl.len().alias("catalog_count"),
+            pl.col("resources").list.len().sum().alias("resource_count")
+        ]) \
+        .collect()
+    
+    catalog_info["total_records"] = catalog_counts_by_organization.select("catalog_count").sum().item()
+    catalog_info["total_resources"] = catalog_counts_by_organization.select("resource_count").sum().item()
+    catalog_info["organizations"] = catalog_counts_by_organization.unnest("organization").to_dicts()
+
+    return catalog_info
+
+def get_catalog_differences(older: pl.LazyFrame, newer: pl.LazyFrame) -> dict:
+
+    # there is an alternate approach: full outer join + filtering, which only
+    # requires one pass through the data, but it may require more memory and time
+    added = newer \
+        .join(older, on="id", how="anti") \
+        .collect()
+    removed = older \
+        .join(newer, on="id", how="anti") \
+        .collect()
+
+    return {
+        "added": added.to_dicts(),
+        "removed": removed.to_dicts()
+    }
 
 logging.debug("functions loaded")
 
 # %%
-# load the data to a local object
-ndson_folder = get_recent_catalog_folder(local_config["input"]["data_folder"])
-ndson_files = get_json_file_list(ndson_folder)
+# get with the work
+folders = get_recent_catalog_folders(local_config["input"]["data_folder"])
 
-ndson_older_folder = get_recent_catalog_folder(local_config["input"]["data_folder"], age=1)
-ndson_older_files = get_json_file_list(ndson_older_folder)
+results = {}
+
+for i in range(len(folders) - 1):
+    logging.debug(f"processing {folders[i]}...")
+    ndjson_files = get_json_file_list(folders[i])
+    ndjson_older_files = get_json_file_list(folders[i + 1])
+
+    catalog = filter_catalog(pl.scan_ndjson(ndjson_files, ignore_errors=True), excluded_organizations=excluded_organizations)
+    catalog_older = filter_catalog(pl.scan_ndjson(ndjson_older_files, ignore_errors=True), excluded_organizations=excluded_organizations)
+
+    results[folders[i]] = {
+        "current_fileset": folders[i],
+        "comparison_fileset": folders[i + 1],
+        "counts": collect_catalog_info(catalog),
+        "deltas": get_catalog_differences(older=catalog_older, newer=catalog)
+    }    
 
 logging.debug("file lists loaded")
 
 # %%
-# scan the relevant files for invalid line breaks
-if check_ndson_for_line_breaks([ndson_folder, ndson_older_folder]):
-    # end the script if any invalid line breaks are found
-    logging.error("Invalid line breaks found in the ndson files.")
-    raise SystemExit
+# save the statistics to ~/Downloads/payload.json (temporary)
+# this will be replaced with a webhook call to github actions
+with open(os.path.join(os.path.expanduser("~/Downloads"),"payload.json"), mode="w") as file:
+    logging.debug(f"saving statistics to {file.name}...")
+    json.dump(results, file)
 
-logging.debug("line breaks checked")
-
-# %%
-# load the data into a polars lazyframes
-# this is done to avoid loading the entire dataset into memory
-catalog = pl.scan_ndjson(ndson_files, ignore_errors=True)
-catalog_older = pl.scan_ndjson(ndson_older_files, ignore_errors=True)
-
-logging.debug("data lazy loaded")
-
-# %%
-# set up filters and queries
-catalog_unique = catalog.unique(subset=["id"])
-catalog_unique_filtered = catalog_unique.filter(
-    ~pl.col("organization").struct.field("id").is_in(excluded_organizations)
-)
-
-catalog_older_unique = catalog_older.unique(subset=["id"])
-catalog_older_unique_filtered = catalog_older_unique.filter(
-    ~pl.col("organization").struct.field("id").is_in(excluded_organizations)
-)
-
-logging.debug("filters set up")
-
-# %%
-# get the number of catalog entries by organization
-catalog_counts_by_organization = (
-    catalog_unique_filtered \
-        .group_by(pl.col("organization").struct.field("id").alias("organization_id")) \
-        .agg(pl.len().alias("catalog_entry_count"))
-).collect()
-
-logging.debug(f"catalog counts by organization: {catalog_counts_by_organization.height}")
-
-# %%
-# collect unique organizations from both datasets
-organizations = catalog_unique_filtered.select("organization").unnest("organization").unique("id").collect()
-logging.debug(f"organizations: {organizations.height}")
-organizations_older = catalog_older_unique_filtered.select("organization").unnest("organization").unique("id").collect()
-logging.debug(f"organizations_older: {organizations_older.height}")
-
-# merge the two lists and de-duplicate
-organizations_merged = pl.concat([organizations, organizations_older]).unique("id")
-
-logging.debug(f"organizations_merged: {organizations_merged.height}")
-
-# %%
-# get the number of records in each catalog
-catalog_unique_filtered_count = catalog_unique_filtered.select(pl.len()).collect().item()
-logging.info(f"latest catalog federal record count: {catalog_unique_filtered_count}")
-
-catalog_older_unique_filtered_count = catalog_older_unique_filtered.select(pl.len()).collect().item()
-logging.info(f"older catalog federal record count: {catalog_older_unique_filtered_count}")
-
-# %% 
-# get the difference between the two catalogs
-
-# Rows in the new snapshot that were not in the old (added rows)
-added = catalog_unique_filtered.join(catalog_older_unique, on="id", how="anti")
-added_df = added.collect()
-logging.info(f"rows added: {added_df.height}")
-
-# Rows in the old snapshot that are missing in the new (removed rows)
-removed = catalog_older_unique_filtered.join(catalog_unique, on="id", how="anti")
-removed_df = removed.collect()
-logging.info(f"rows removed: {removed_df.height}")
-
-# %%
-# save the statistics
 logging.info("statistics saved")
 
 # %%
