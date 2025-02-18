5afe7868bb7423b870faa2e21ba1729da9494c52
technickle <github@nicklin.info>
2025-02-18 10:58:32 -0500

    use ISO8601 for datetimes in json

--- scripts/generate_web_data/generate_web_data.py
+++ scripts/generate_web_data/generate_web_data.py
@@ -2,6 +2,7 @@
 # imports and initialization
 import copy
 # import csv
+import datetime
 import json
 import logging
 import os
@@ -40,32 +41,27 @@ for json_file in daily_json_files:
     with open(os.path.join(local_config["input"]["data_folder"], json_file), "r", encoding="utf-8") as f:
         daily_data = json.load(f)
 
-    current_date = daily_data["date"]
+    current_date = date_obj = datetime.datetime \
+        .strptime(daily_data["date"], "%Y%m%dT%H%M%S") \
+        .replace(tzinfo=datetime.timezone.utc)
+    
+    current_date_iso_string = current_date.isoformat()
 
     for org in daily_data["counts"]["organizations"]:
         if org["id"] not in organizations:
             output_org = copy.deepcopy(org)
-            output_org["catalog_entry_counts"] = []
-            output_org["resource_entry_counts"] = []
+            output_org["catalog_entry_counts"] = {}
+            output_org["resource_entry_counts"] = {}
             del output_org["catalog_count"]
             del output_org["resource_count"]
             organizations[org["id"]] = output_org
         else:
             output_org = organizations[org["id"]]
 
-        output_org["catalog_entry_counts"].append(
-            {
-                "date": current_date,
-                "count": org["catalog_count"]
-            }
-        )
+        output_org["catalog_entry_counts"][current_date_iso_string] = org["catalog_count"]
+        output_org["resource_entry_counts"][current_date_iso_string] = org["resource_count"]
 
-        output_org["resource_entry_counts"].append(
-            {
-                "date": current_date,
-                "count": org["resource_count"]
-            }
-        )
+    
 
 # sort organizations by organization.title property
 organizations = dict(sorted(organizations.items(), key=lambda x: x[1]["title"]))
@@ -73,7 +69,16 @@ organizations = dict(sorted(organizations.items(), key=lambda x: x[1]["title"]))
 # %%
 # write the web data to the output folder
 
+excluded_keys = ['timeline', 'city']
+
 # write the organizations file
 with open(os.path.join(local_config["output"]["web_data_folder"], "organizations.json"), "w", encoding="utf-8") as f:
-    json.dump(list(organizations.values()), f, ensure_ascii=False)
+
+    filtered_dict = {k: v for k, v in organizations.items() if k not in excluded_keys}
+    json.dump(list(filtered_dict.values()), f, ensure_ascii=False)
+
+for id, org in organizations.items():
+    with open(os.path.join(local_config["output"]["web_data_folder"], "organizations", f"{id}.json"), "w", encoding="utf-8") as f:
+        json.dump(org, f, ensure_ascii=False)
+
 # %%
