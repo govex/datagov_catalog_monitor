6031d9d34e3767286af06ecc0fd171d9849561e0
technickle <github@nicklin.info>
2025-02-28 23:18:45 -0500

    updated web data script

--- scripts/generate_web_data/generate_web_data.py
+++ scripts/generate_web_data/generate_web_data.py
@@ -23,35 +23,14 @@ local_config = {
     }
 }
 
-sample_output = {
-    "last_updated": datetime.datetime.now(datetime.timezone.utc).isoformat(),
-    "organization_count": 0,
-    "catalog_daily_statistics": {
-        "2020-01-01T00:00:00Z": {
-            "added": 0,
-            "removed": 0,
-            "count": 0,
-            "net_change": 0
-        }
-    },
-    "resource_daily_statistics": {
-        "2020-01-01T00:00:00Z": {
-            "added": 0,
-            "removed": 0,
-            "total_count": 0,
-            "total_delta": 0
-        }
-    },
-    "organizations": []
-}
-
 output = {
     "last_updated": datetime.datetime.now(datetime.timezone.utc).isoformat(),
-    "organizations": {}
+    "organizations": {},
+    "catalog_daily_statistics": [],
+    "resource_daily_statistics": []
 }
 
 excluded_organization_keys = ['timeline', 'city']
-
 
 logging.debug("Initialized")
 
@@ -64,6 +43,11 @@ logging.info(f"Found {len(daily_json_files)} daily json files")
 
 # %%
 # iterate through the daily json files and generate the web data
+previous_catalog_count_by_org = {}
+previous_resource_count_by_org = {}
+previous_catalog_count = None
+previous_resource_count = None
+
 for json_file in daily_json_files:
     logging.info(f"Processing {json_file}")
     with open(os.path.join(local_config["input"]["data_folder"], json_file), "r", encoding="utf-8") as f:
@@ -75,23 +59,70 @@ for json_file in daily_json_files:
     
     current_date_iso_string = current_date.isoformat()
 
+    # create a data object that counts daily_data["deltas"]["added"] and daily_data["deltas"]["removed  "] by organization
+    added_by_org = {}
+    for delta_added in daily_data.get("deltas", {}).get("added", []):
+        added_by_org[delta_added["organization"]["id"]] = added_by_org.get(delta_added["organization"]["id"], 0) + 1
+
+    removed_by_org = {}
+    for delta_removed in daily_data.get("deltas", {}).get("removed", []):
+        removed_by_org[delta_removed["organization"]["id"]] = removed_by_org.get(delta_removed["organization"]["id"], 0) + 1
+
+    # TODO: the deltas don't align with the added and removed counts; need to find out why
+
+    # get the total count of datasets and resources
+    output["catalog_daily_statistics"].append({
+        "t": current_date_iso_string,
+        "n": daily_data["counts"]["total_records"],
+        "Δ": daily_data["counts"]["total_records"] - previous_catalog_count if previous_catalog_count is not None else 0,
+        "↑": sum(added_by_org.values()),
+        "↓": sum(removed_by_org.values())
+    })
+
+    output["resource_daily_statistics"].append({
+        "t": current_date_iso_string,
+        "n": daily_data["counts"]["total_resources"],
+        "Δ": daily_data["counts"]["total_resources"] - previous_resource_count  if previous_resource_count is not None else 0,
+        "↑": sum(added_by_org.values()),
+        "↓": sum(removed_by_org.values())
+    })
+
+    # save the previous values for the next iteration
+    previous_catalog_count = daily_data["counts"]["total_records"]
+    previous_resource_count = daily_data["counts"]["total_resources"]
+
     for org in daily_data["counts"]["organizations"]:
-        if org["id"] not in output["organizations"]:
-            output_org = { k: v for k, v in org.items() if k not in excluded_organization_keys }
-            output_org["catalog_entry_counts"] = {}
-            output_org["resource_entry_counts"] = {}
+        output_org = output["organizations"].get(org["id"])
+        if output_org is None:
+            output_org = {k: v for k, v in org.items() if k not in excluded_organization_keys}
+            output_org["catalog_entry_counts"] = []
+            output_org["resource_entry_counts"] = []
             del output_org["catalog_count"]
             del output_org["resource_count"]
             output["organizations"][org["id"]] = output_org
-        else:
-            output_org = output["organizations"][org["id"]]
 
-        output_org["catalog_entry_counts"][current_date_iso_string] = org["catalog_count"]
-        output_org["resource_entry_counts"][current_date_iso_string] = org["resource_count"]
+        output_org["catalog_entry_counts"].append({
+            "t": current_date_iso_string,
+            "n": org["catalog_count"],
+            "Δ": org["catalog_count"] - previous_catalog_count_by_org[org["id"]] if org["id"] in previous_catalog_count_by_org else 0,
+            "↑": added_by_org[org["id"]] if org["id"] in added_by_org else 0,
+            "↓": removed_by_org[org["id"]] if org["id"] in removed_by_org else 0
+        })
+
+        output_org["resource_entry_counts"].append({
+            "t": current_date_iso_string,
+            "n": org["resource_count"],
+            "Δ": org["resource_count"] - previous_resource_count_by_org[org["id"]] if org["id"] in previous_resource_count_by_org else 0,
+            "↑": added_by_org[org["id"]] if org["id"] in added_by_org else 0,
+            "↓": removed_by_org[org["id"]] if org["id"] in removed_by_org else 0
+        })
+
+        previous_catalog_count_by_org[org["id"]] = org["catalog_count"]
+        previous_resource_count_by_org[org["id"]] = org["resource_count"]
 
     total_count = daily_data["counts"]["total_records"]
-    additions = len(daily_data.get("deltas", {}).get("added", []))
-    deletions = len(daily_data.get("deltas", {}).get("removed", []))
+    additions = sum(added_by_org.values())
+    deletions = sum(removed_by_org.values())
 
 
     logging.info(f"Total Datasets: { total_count }; Deletions: { deletions }; Additions: { additions}")
@@ -106,10 +137,10 @@ output["organizations"] = dict(sorted(output["organizations"].items(), key=lambd
 
 # write the organizations file
 with open(os.path.join(local_config["output"]["web_data_folder"], "organizations.json"), "w", encoding="utf-8") as f:
-    json.dump(output, f, ensure_ascii=False)
+    json.dump(output, f, ensure_ascii=False, indent=2, sort_keys=True)
 
 for id, org in output["organizations"].items():
     with open(os.path.join(local_config["output"]["web_data_folder"], "organizations", f"{id}.json"), "w", encoding="utf-8") as f:
-        json.dump(org, f, ensure_ascii=False)
+        json.dump(org, f, ensure_ascii=False, indent=2, sort_keys=True)
 
 # %%
